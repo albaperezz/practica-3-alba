80c97e886a03410f332d37498033a193b198710f
RubenMorenoAlmancio <17vocaloid@gmail.com>
2021-09-29 15:51:13 +0200

    pequeño cambio
    se añade una frase

--- bin2hex.py
+++ bin2hex.py
@@ -2,6 +2,7 @@
 # Convierte un número binario a hexadecimal.
 # El número binario es un string e.g. "101"
 # El número hexadecimal es también un string e.g. "AB5"
+# Hay otros ficheros disponibles.
 # ------------------------------------------------------
 
 def TablabinHex(cuatro_digitos_binarios):
